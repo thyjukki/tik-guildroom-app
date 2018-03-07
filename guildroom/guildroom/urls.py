e9fdbc2c111f9bba463dfd00217095771cbeccb6
Jussi Joki <judejoki@gmail.com>
2018-03-07 22:06:56 +0200

    Initial project

---
+++ guildroom/guildroom/urls.py
@@ -0,0 +1,20 @@
+"""
+Definition of urls for guildroom.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
