47829504cfe88918226bdb03c46a4c90c79f425c
Jussi Joki <judejoki@gmail.com>
2018-03-08 08:51:16 +0200

    Basic music playing

--- guildroom/tikplay/models.py
+++ guildroom/tikplay/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Song:
+    url = models.URLField()
+    audio_url = models.URLField()
+    title = models.CharField()
--- guildroom/tikplay/views.py
+++ guildroom/tikplay/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+import pafy
+import vlc
 
 from tikplay.forms import YoutubeForm
 
 # Create your views here.
 def add_song(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         form = YoutubeForm(request.POST)
 
         if form.is_valid():
-            youtube_url = form.youtube_url
+            youtube_url = form.cleaned_data['youtube_url']
+            video = pafy.new(youtube_url)
+            audio_url = video.getbestaudio().url
+            print (audio_url)
+            player = vlc.MediaPlayer(audio_url)
+            player.play()
     else:
         form = YoutubeForm()
 
