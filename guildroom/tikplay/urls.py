b74ae5e7dd7d4446246f5447ed72ea4c2c1a5b36
Jussi Joki <judejoki@gmail.com>
2018-03-12 12:41:39 +0200

    Added api for adding songs

--- guildroom/tikplay/urls.py
+++ guildroom/tikplay/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^api/queue/$', views.get_queue, name='api-queue'),
     url(r'^api/current/$', views.get_current, name='api-current'),
     url(r'^api/pop/$', views.pop_current, name='api-pop'),
+    url(r'^api/add/$', views.add_song, name='api-add'),
 ]
