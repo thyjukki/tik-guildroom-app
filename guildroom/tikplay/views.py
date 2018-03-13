838d66a7beec733f7071d90705f92892d4c9af75
Jussi Joki <judejoki@gmail.com>
2018-03-14 00:45:01 +0200

    Headers

--- guildroom/tikplay/views.py
+++ guildroom/tikplay/views.py
@@ -11,6 +11,7 @@ from tikplay.decorators import jsonp
 from tikplay import playerCommandQueue
 
 import json
+from requests import get
 # Create your views here.
 
 @api_view(['POST', 'GET'])
@@ -48,7 +49,8 @@ def add_song_view(request):
         form = YoutubeForm()
          
     song_list = Song.objects.all()
-    return render(request, 'add_song.html', {'form': form, 'song_list': song_list})
+    ip = get('https://api.ipify.org').text
+    return render(request, 'add_song.html', {'form': form, 'song_list': song_list, 'ip': ip})
 
 @api_view(['GET'])
 @jsonp
