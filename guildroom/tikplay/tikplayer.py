dab31792201c2a9adf14f64ce1b45d23217ae737
Jussi Joki <judejoki@gmail.com>
2018-03-20 17:02:52 +0200

    Removed some debug info

--- guildroom/tikplay/tikplayer.py
+++ guildroom/tikplay/tikplayer.py
@@ -19,12 +19,10 @@ class TiKPlayer ():
         events.event_attach(vlc.EventType.MediaPlayerEndReached, self.song_finished)
         self.playing = True
     def run(self):
-        print ("Starting player")
         self.set_current_song()
         self.play()
 
     def set_current_song(self):
-        print("set_current_song")
         try:
             song = Song.objects.earliest()
         except:
@@ -36,7 +34,6 @@ class TiKPlayer ():
         self.player.set_media(vlc.Media(song.audio_url))
 
     def song_finished(self, event):
-        print("Song finished")
         newSong = pop()
         if newSong:
             threading.Thread(target=self.new).start()
@@ -46,22 +43,18 @@ class TiKPlayer ():
         self.player.play()
     
     def new(self):
-        print("New")
         self.stop()
         self.set_current_song()
         self.play()
 
     def pause(self):
-        print("Pause")
         self.playing = False
         self.player.pause()
 
     def clear(self):
-        print("Clear")
         self.stop()
 
     def stop(self):
-        print("Stop")
         self.player.stop()
 
     def set_volume(self, volume):
