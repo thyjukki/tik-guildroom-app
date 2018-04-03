4063a90429aad8c905bee85dfdabcce3e76ea925
Jussi Joki <judejoki@gmail.com>
2018-04-04 01:56:26 +0300

    Vitu numpy

--- kiltacam2/kiltacam2/kiltacam2.py
+++ kiltacam2/kiltacam2/kiltacam2.py
@@ -92,15 +92,17 @@ while True:
         try:
             img = cam.getFrame()
 
-            if img:
-                if (index in flip_cams):
-                    img = cv2.flip( img, -1 )
-                img = insertTimestamp(img)
-                cv2.imwrite("last.jpg",img)
+            if img is None:
+                continue
 
-                baseUrl = 'http://{}/cam/api/set'.format(host)
-                files = {"current": open('last.jpg', 'rb')}
-                res = requests.post(baseUrl, {"position": index, "token": token}, files=files)
+            if (index in flip_cams):
+                img = cv2.flip( img, -1 )
+            img = insertTimestamp(img)
+            cv2.imwrite("last.jpg",img)
+
+            baseUrl = 'http://{}/cam/api/set'.format(host)
+            files = {"current": open('last.jpg', 'rb')}
+            res = requests.post(baseUrl, {"position": index, "token": token}, files=files)
         except Exception as e:
             print("Camera by index {}: {}".format(index, e))
     time.sleep(30)
